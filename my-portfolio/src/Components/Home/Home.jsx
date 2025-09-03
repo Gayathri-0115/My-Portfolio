7cd5eef6cd5d6466c45c075ff713a15978b16d88
Gayathri-0115 <gayathri0162005@gmail.com>
2025-09-03 11:42:55 +0530

    minor changes

--- my-portfolio/src/Components/Home/Home.jsx
+++ my-portfolio/src/Components/Home/Home.jsx
@@ -9,7 +9,10 @@ const Home = () => {
         <h2>Hi, I'm <b>Gayathri</b></h2>
         <p>An Aspiring <b className='main'>MERN Full Stack Developer </b>passionate about building clean, Responsive and user-friendly Web Applications.</p>
         <div className="btns">
-          <button className='github'><i className="fa-brands fa-github "></i></button>
+          <a href="https://github.com/Gayathri-0115/">
+            <button className='github'><i className="fa-brands fa-github "></i></button>
+          </a>
+          
           <button className='linkedin'><i className="fa-brands fa-linkedin"></i></button>
           <AnchorLink href='#contact' className='anchorlink'><button className="connect">Connect with me </button></AnchorLink>
         </div>
